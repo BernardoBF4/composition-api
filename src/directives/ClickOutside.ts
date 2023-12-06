7f19080fd83dbc2b1ef55d716ab3db4436a090a0
BernardoBF4 <bernardo.fantin@gmail.com>
2023-12-06 08:57:52 -0300

    🚑 fix(vue: composition api): tipagem corrigida

--- src/directives/ClickOutside.ts
+++ src/directives/ClickOutside.ts
@@ -1,13 +1,17 @@
+type Element = HTMLElement & {
+  __ClickOutsideHandler__: (event: MouseEvent) => void
+}
+
 export default {
-  mounted(el, binding: any) {
-    el.__ClickOutsideHandler__ = (event) => {
-      if (!(el === event.target || el.contains(event.target))) {
+  mounted(el: Element, binding: any) {
+    el.__ClickOutsideHandler__ = (event: MouseEvent) => {
+      if (!(el === event.target || el.contains(event.target as Node))) {
         binding.value()
       }
     }
     document.body.addEventListener('click', el.__ClickOutsideHandler__)
   },
-  unmounted(el) {
+  unmounted(el: Element) {
     document.body.removeEventListener('click', el.__ClickOutsideHandler__)
   },
 }
