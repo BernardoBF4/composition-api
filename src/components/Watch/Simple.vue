982b363d0d6b03c917eba55d8e90203990c8e476
BernardoBF4 <bernardo.fantin@gmail.com>
2023-12-06 09:25:04 -0300

    ✨ feat(app: watch): adicionado mais um exemplo de watch

--- src/components/Watch/Simple.vue
+++ src/components/Watch/Simple.vue
@@ -12,21 +12,17 @@ import { ref, watch } from 'vue'
 const value = ref(0)
 const text = ref(null)
 
+watch(value, (new_value, old_value) => {
+  console.log('#############')
+  console.log('Velho:', old_value)
+  console.log('Novo:', new_value)
+})
+
 watch(value, () => {
   const emojis = ['🫥', '😶‍🌫️', '😰', '😦', '😧', '😯', '😮']
-
-  if (value === 1) {
-  }
 
   text.value.innerText = `O valor acabou de mudar de novo ${
     emojis[Math.floor(Math.random() * emojis.length)]
   }`
 })
 </script>
-
-<style scoped>
-.flex {
-  display: flex;
-  flex-direction: column;
-}
-</style>
